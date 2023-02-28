40fbc366b6131dc92866881735f6f4ac93a20170
CobySena <54821256+CobySena@users.noreply.github.com>
2023-02-27 19:14:22 -0700

    Add files via upload

---
+++ HW5/readme.md
@@ -0,0 +1,1 @@
+Starting working on the memory game.
---
+++ HW5/script/main.js
@@ -0,0 +1,32 @@
+var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10"];
+var blankImagePath = "images/Question_Mark.png";
+var actualImages = new Array();
+
+function printBlanks()
+{
+    createRandomImageArray();
+    for(var i = 0; i < imageTags.length; i++)
+    {
+        document.getElementById(imageTags[i]).src = blankImagePath;
+    }
+}
+
+function createRandomImageArray()
+{
+    var actualImagePath = ["images/Tomoe.jpg", "images/Omega.png", "images/Ouroboros.png", "images/Celtic_Knot.jpg", "images/yin-yang.jpg"];
+    var count = [0,0,0,0,0];
+    while(actualImages.length < 10)
+    {
+        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
+        if(count[randomNumber] < 2)
+        {
+            actualImages.push(actualImagePath[randomNumber]);
+            count[randomNumber] = count[randomNumber] + 1;
+        }
+    }
+}
+
+function flipImage(number)
+{
+    document.getElementById(imageTags[number]).src = actualImages[number];
+}
